6569cead0fd06664d2050940f74fb0ae88bb2c4f
GuilhermeRossiKirsten <133013119+GuilhermeRossiKirsten@users.noreply.github.com>
2025-08-08 14:35:51 +0000

    feat: create and use InternalServerError in /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,38 +1,45 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
-async function status(resquest, response) {
+async function status(request, response) {
   //const updateAt = Date.now();
+  try {
+    const updateAt = new Date().toISOString();
 
-  const updateAt = new Date().toISOString();
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+    const databaseMaxConnectionResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const databaseMaxConnectionValue = parseInt(
+      databaseMaxConnectionResult.rows[0].max_connections,
+    );
 
-  const databaseMaxConnectionResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionValue = parseInt(
-    databaseMaxConnectionResult.rows[0].max_connections,
-  );
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+      values: [databaseName],
+    });
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count;
 
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
-    values: [databaseName],
-  });
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
-
-  response.status(200).json({
-    updated_at: updateAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: databaseMaxConnectionValue,
-        opened_connections: databaseOpenedConnectionsValue,
+    response.status(200).json({
+      updated_at: updateAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: databaseMaxConnectionValue,
+          opened_connections: databaseOpenedConnectionsValue,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    console.log(error);
+    const publicErrorObject = new InternalServerError({ cause: error });
+    console.error(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
