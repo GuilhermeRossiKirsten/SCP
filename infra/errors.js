393682190e20e02966d1b9c2c316c36dd10e5f5e
GuilhermeRossiKirsten <133013119+GuilhermeRossiKirsten@users.noreply.github.com>
2025-10-09 14:09:51 +0000

    feat: implement user registration with validation and error handling

--- infra/errors.js
+++ infra/errors.js
@@ -17,3 +17,23 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação aconteceu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 409;
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      name: this.name,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
